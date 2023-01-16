5c3ddd3fef38b75dacd0e74d7ef08fcd03c17fdf
Yulius Pratama <yuliuspratama9e@gmail.com>
2023-01-16 12:32:29 +0700

    init Web Prototype

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Components/Footer.jsx'
+import HeaderNav from './Components/Header.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <HeaderNav></HeaderNav>
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
